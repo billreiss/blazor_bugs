f19689b79ca18c333a83a6d83c7a4e1a2d85535c
Bill Reiss <bireis@microsoft.com>
2023-12-11 14:15:36 -0500

    Sample for Email optional input Blazor

---
+++ ValidatingUserInput/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValidatingUserInput.Models
+{
+	public class Person
+	{
+		[Required]
+		public string Name { get; set; }
+		[Range(18, 80, ErrorMessage = "Age must be between 18 and 80.")]
+		public int Age { get; set; }
+
+		[EmailAddress]
+		public string EmailAddress { get; set; }
+	}
+}
---
+++ ValidatingUserInput/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ValidatingUserInput
+{
+	public static class Program
+	{
+		public async static Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("app");
+
+			await builder.Build().RunAsync();
+		}
+	}
+}
